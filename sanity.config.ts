bb25f2730c4d43f077a1111014fab7e05d8ea0c9
Courtney Nguyen <hello@courtyen.io>
2024-06-16 22:02:54 -0600

    blog root and sanity structure explorer

--- sanity.config.ts
+++ sanity.config.ts
@@ -19,20 +19,38 @@ if (!projectId || !dataset) {
 
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-// import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schema";
 import { colorInput } from "@sanity/color-input";
 import { MyCustomActions } from "./documentActions/pageAction";
-import { structure } from "./src/structure";
+import { CustomStructure } from "./src/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
+import { FaCar, FaTshirt, FaGavel, FaPage4 } from "react-icons/fa";
 
 export default defineConfig({
   name: "engsta-rides",
   title: "Engsta Rides",
   projectId,
   dataset,
-  plugins: [structureTool({ structure }), visionTool(), colorInput(), markdownSchema()],
+  plugins: [
+    structureTool({
+      structure: (S, context) => {
+        console.log(context); // returns { currentUser, dataset, projectId, schema, getClient, documentStore }
+        return S.list()
+          .title("Shop")
+          .items([
+            S.documentTypeListItem("shirt").title("Shirts").icon(FaTshirt),
+            S.documentTypeListItem("accessory").title("Swag").icon(FaCar),
+            S.documentTypeListItem("auction").title("Auctions").icon(FaGavel),
+            S.divider(),
+            S.documentTypeListItem("blog").title("Blog").icon(FaPage4),
+          ]);
+      },
+    }),
+    visionTool(),
+    colorInput(),
+    markdownSchema(),
+  ],
   document: {
     actions: MyCustomActions,
   },
--- src/structure/index.ts
+++ src/structure/index.ts
@@ -1,3 +1,5 @@
+import { FaCar } from 'react-icons/fa'
+
 export const structure = (S: any) =>
   S.list()
     .title("Content")
@@ -16,3 +18,13 @@ export const structure = (S: any) =>
           !["shirtVariant", "shirtColor"].includes(listItem.getId()),
       ),
     ]);
+
+export const CustomStructure = () => ({
+  name: "shop",
+  title: "Shop",
+  icon: FaCar,
+  structure: (S: any) => S.documentTypeListItems().filter(
+      (listItem: any) =>
+        !["shirtVariant", "shirtColor"].includes(listItem.getId()),
+    )
+})
